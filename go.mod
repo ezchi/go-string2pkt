f56924e1282a0786dcbd5aa74d886a7b54ca6833
Enze Chi <Enze.Chi@gmail.com>
2020-03-08 08:42:07 +1100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ezchi/go-string2gkt
+
+go 1.14
+
+require github.com/google/gopacket v1.1.17
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 2 {
+
+		packetData, err := hex.DecodeString(os.Args[1])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		packet := gopacket.NewPacket(packetData, layers.LayerTypeEthernet, gopacket.Default)
+
+		fmt.Println(packet)
+	} else {
+		fmt.Printf("Usage: %s <byte string>\n", os.Args[0])
+	}
+}
+
+// Local Variables:
+// go-run-args: "28993A8F3C49001C73B57BC308004500009CA8BD40004006B5CE0A2A632C0A2A6450C1AC401D837BB94DFACDD51C5018FFFFB2BA00000072554F4D41454749414145413239333636134900CA5300000000000000040020D2580300373832323233202020202020202020204145474941414541202020204D2020202020202020202020202020202020202020202020202020202020202020202000000000000000000000590000000000"
+// End:
